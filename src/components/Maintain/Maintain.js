968453ac114159f87d2baa87ffbf71216f429879
Trần Nhật Duy <duytn.hcm@gmail.com>
2019-08-18 17:13:43 +0700

    Add water css

--- src/components/Maintain/Maintain.js
+++ src/components/Maintain/Maintain.js
@@ -6,7 +6,10 @@ const Maintain = () => (
     <h1>Website đang được xây dựng.</h1>
     <h3>Vui lòng truy cập lại sau.</h3>
 
-    <img src="https://media.giphy.com/media/3o7abDnUPDkOC05QU8/giphy.gif" />
+    <img
+      src="https://media.giphy.com/media/3o7abDnUPDkOC05QU8/giphy.gif"
+      alt="The worker working."
+    />
 
     <p>
       &copy; {new Date().getFullYear()} - make with <span className="heart" />{' '}
