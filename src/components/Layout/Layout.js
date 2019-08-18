f88058e0b48d0c27340cd069a597f29de2aeb7a4
Trần Nhật Duy <duytn.hcm@gmail.com>
2019-08-18 17:40:54 +0700

    Fix somethings

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import { useStaticQuery, graphql } from 'gatsby'
 
 import Header from '../Header/Header'
-import './Layout.css'
+import './Layout.scss'
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
