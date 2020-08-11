bd78ca7a161c57d8e913c8017cc86200efb61185
Trần Nhật Duy <duytn.hcm@gmail.com>
2020-08-11 16:14:58 +0700

    Fix

--- frontend/src/constants/app.js
+++ frontend/src/constants/app.js
@@ -5,7 +5,7 @@ export const domain =
 
 export const siteName = `Khắc dấu Hữu Hạ - TP. Mỹ Tho - Tiền Giang`;
 
-export const siteDescription = `Khắc dâu Hữu Hạ với 20 năm kinh nghiệm, chuyên khắc các loại dấu tên, dấu chức danh, dấu tròn danh nghiệp,... cũng như cung cấp các vật liệu ngành dấu như mực dấu, tampon dấu,... Bên cạnh đó, cơ sở cũng cung nhận khắc chữ meka, làm bảng hiệu, bảng đeo tên, thẻ đại hội và cung cấp tượng bác bằng thạch cao, các loai cờ trang trí cho cơ quan, tổ chức doanh nghiệp.`;
+export const siteDescription = `Khắc dấu Hữu Hạ với 20 năm kinh nghiệm, chuyên khắc các loại dấu tên, dấu chức danh, dấu tròn danh nghiệp,... cũng như cung cấp các vật liệu ngành dấu như mực dấu, tampon dấu,... Bên cạnh đó, cơ sở cũng cung nhận khắc chữ meka, làm bảng hiệu, bảng đeo tên, thẻ đại hội và cung cấp tượng bác bằng thạch cao, các loai cờ trang trí cho cơ quan, tổ chức doanh nghiệp.`;
 
 export const siteImage = `${domain}/images/seo.png`;
 
